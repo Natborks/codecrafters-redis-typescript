3a24dbb05b8fd7bbca18de1f669160f9acb169ca
natborks <nathanael@moonsquare.co>
2026-04-11 18:49:48 -0230

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -9,7 +9,7 @@ console.log("Logs from your program will appear here!");
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
   connection.on('data', (data: Buffer) => {
-    console.log(data.toString())
+    console.log("DATA is: ",data.toString())
 
 
     const scanner = new Scanner(data.toString("utf8"))
