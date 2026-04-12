127bba7d54bc8ba923fd7b93fdeeab9070f11c88
natborks <nathanael@moonsquare.co>
2026-04-12 17:04:49 -0230

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -26,7 +26,7 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
         break
       case "GET":
         const query = args[1]
-        console.log(query)
+        console.log(args)
         const result = cache.get(query)
         if (result) {
           connection.write(writeBulkString([result])) 
