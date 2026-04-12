9e8aa68a6717c779bfded99c2d123de5b2b9e03f
natborks <nathanael@moonsquare.co>
2026-04-11 21:48:54 -0230

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,22 +1,31 @@
 import * as net from "net";
-import Scanner from "./scanner/Scanner";
+import Scanner from "./parser/Scanner";
 import { TokenType } from "./types/TokenType";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const cache = new Map()
+
 // Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
   connection.on('data', (data: Buffer) => {
-    if (data.toString().includes("PING")) {
+    const request = data.toString()
+
+    const [command, ...args] = request.split(/\s+/)
+
+    if (command === 'PING') {
       return connection.write("+PONG\r\n")
     }
 
-    const scanner = new Scanner(data.toString("utf8"))
+    if (data.toString().includes("GET")) {
+      
+    }
+
 
     let response = "$"
-    for (const token of scanner.scanTokens()) {
+    for (const token of args) {
       if (token.type === TokenType.STRING) {
         if (token.literal === "ECHO") continue
 
---
+++ app/parser/Parser.ts
@@ -0,0 +1,7 @@
+export default class Parser {
+    #source
+
+    constructor(souce: string) {
+        this.#source = souce    
+    }
+}
