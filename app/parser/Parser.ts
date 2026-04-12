1ebd050b648bdad61d96238970462ad8f653a9fb
natborks <nathanael@moonsquare.co>
2026-04-12 11:35:56 -0230

    codecrafters submit [skip ci]

--- app/parser/Parser.ts
+++ app/parser/Parser.ts
@@ -2,24 +2,24 @@ import { TokenType } from "../types/TokenType"
 import Scanner from "./Scanner"
 import Token from "./Token"
 
-export type ParsedData = [
-    command: string,
-    args: string[] | string
-]
-
 export default class Parser {
     #source
 
     constructor(source: string) {
         this.#source = source    
-    }
-
-    parseCommand() : ParsedData {
         const scanner = new Scanner(this.#source)
         const tokens = scanner.scanTokens()
-
         const commandType = tokens.at(0)?.lexeme
-        
+
+        if (commandType === undefined) {
+            throw new Error("No command type found")
+        }
+    
+        this.parseCommand(commandType, tokens, [])
+    }
+
+    private parseCommand(commandType: string, tokens: Token[], acc: string[]) : sting[] {
+
         if (commandType === "+") {
             const command = tokens.at(1)?.lexeme
             if (command === undefined) {
@@ -28,6 +28,21 @@ export default class Parser {
 
             const args = this.parseSimpleString(tokens.slice(2))
             return [command, args]
+        }
+
+        if (commandType === "*") {
+            const commandType = tokens.at(4)?.lexeme
+            
+            if(!commandType) throw new Error("Missing command token")
+
+            return this.parseCommand(commandType, tokens.slice(5), acc)
+        }
+
+        if (commandType === "$") {
+
+            const result = this.parseBulkString(tokens.slice(3))
+            
+            return [...acc, ...result]
         }
 
         throw new Error("Unsupported command type")
@@ -43,6 +58,19 @@ export default class Parser {
        //string part of RESP was empty 
        return ""
     }
+
+    private parseBulkString(tokens: Token[]) : string[] {
+        const result = []
+        for (const token of tokens) {
+            if (token.type === TokenType.STRING) {
+                result.push(token.literal)
+            }
+        }
+
+        return result
+    }
+
+
 
 
 }
