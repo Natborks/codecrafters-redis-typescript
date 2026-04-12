c0ee34bd6a6d168038a6dc1ab54d6438b322aa96
natborks <nathanael@moonsquare.co>
2026-04-11 22:52:48 -0230

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,44 +1,37 @@
 import * as net from "net";
-import Scanner from "./parser/Scanner";
 import { TokenType } from "./types/TokenType";
+import Parser from "./parser/Parser";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
-const cache = new Map()
 
 // Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
   connection.on('data', (data: Buffer) => {
-    const request = data.toString()
-
-    const [command, args] = request.split(/\s+/)
-    console.log("ARGS", args)
-
-    if (command === 'PING') {
-      return connection.write("+PONG\r\n")
+    const parser = new Parser(data.toString())
+    const [command, args] = parser.parseCommand()
+    console.log("COMMAND: ",command)
+    switch (command.toUpperCase()){
+      case "PING":
+        connection.write("+PONG\r\n")
+        break
+      case "ECHO":
+        connection.write(writeBulkString(args as string[]))
+        break
     }
-
-    if (data.toString().includes("GET")) {
-      
-    }
-
-
-    let response = "$"
-    for (const token of [...args]) {
-      if (token.type === TokenType.STRING) {
-        if (token.literal === "ECHO") continue
-
-        const literal = token.literal
-        const length = literal.length
-        
-        response = response.concat(`${length}\r\n${literal}\r\n`)
-      }
-    }
-    connection.write(response)
   })
 
 });
+
+function writeBulkString(args: string[]) : string {
+   let response = "$"
+    for (const literal of args) {
+      const length = literal.length
+      response = response.concat(`${length}\r\n${literal}\r\n`)
+    }
+
+    return response
+}
 
 server.listen(6379, "127.0.0.1");
--- app/parser/Parser.ts
+++ app/parser/Parser.ts
@@ -1,7 +1,48 @@
+import { TokenType } from "../types/TokenType"
+import Scanner from "./Scanner"
+import Token from "./Token"
+
+export type ParsedData = [
+    command: string,
+    args: string[] | string
+]
+
 export default class Parser {
     #source
 
-    constructor(souce: string) {
-        this.#source = souce    
+    constructor(source: string) {
+        this.#source = source    
     }
+
+    parseCommand() : ParsedData {
+        const scanner = new Scanner(this.#source)
+        const tokens = scanner.scanTokens()
+
+        const commandType = tokens.at(0)?.lexeme
+        
+        if (commandType === "+") {
+            const command = tokens.at(1)?.lexeme
+            if (command === undefined) {
+                throw new Error("Missing command token")
+            }
+
+            const args = this.parseSimpleString(tokens.slice(2))
+            return [command, args]
+        }
+
+        throw new Error("Unsupported command type")
+    }
+
+    private parseSimpleString(tokens: Token[]) : string {
+       for (const token of tokens) {
+            if (token.type === TokenType.STRING) {
+                return token.literal
+            }
+       } 
+
+       //string part of RESP was empty 
+       return ""
+    }
+
+
 }
