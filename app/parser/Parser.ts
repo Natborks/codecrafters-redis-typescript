9159e8e79c85935ca85c99d2781e9759494efa09
natborks <nathanael@moonsquare.co>
2026-04-18 21:20:08 -0230

    codecrafters submit [skip ci]

--- app/parser/Parser.ts
+++ app/parser/Parser.ts
@@ -18,78 +18,13 @@ export default class Parser {
         this.#source = source    
         const scanner = new Scanner(this.#source)
         const tokens = scanner.scanTokens()
-        // const commandType = tokens.at(0)?.lexeme
         this.#parsedData = tokens
 
-        // if (commandType === undefined) {
-        //     throw new Error("No command type found")
-        // }
-    
-        // this.#parsedData = this.parseCommand(commandType, tokens, [])
     }
-
-    // private parseCommand(commandType: string, tokens: Token[], acc: string[]) : string[] {
-
-    //     if (commandType === SIMPLE_STRING) {
-    //         const command = tokens.at(1)?.lexeme
-    //         if (command === undefined) {
-    //             throw new Error("Missing command token")
-    //         }
-
-    //         const args = this.parseSimpleString(tokens.slice(SIMPLE_STRING_OFFSET))
-    //         return [command, args]
-    //     }
-
-    //     if (commandType === ARRAY) {
-    //         const commandType = tokens.at(4)?.lexeme
-            
-    //         if(!commandType) throw new Error("Missing command token")
-
-    //         return this.parseCommand(commandType, tokens.slice(ARRAY_OFFSET), acc)
-    //     }
-
-    //     if (commandType === BULK_STRING) {
-
-    //         const result = this.parseBulkString(tokens.slice(BULK_STRING_OFFSET))
-            
-    //         return [...acc, ...result]
-    //     }
-
-    //     throw new Error("Unsupported command type")
-    // }
 
     getParsedString() {
         console.log("Parsed: ",this.#parsedData)
         return this.#parsedData
-    }
-
-    private parseSimpleString(tokens: Token[]) : string {
-       for (const token of tokens) {
-            if (token.type === TokenType.STRING) {
-                return token.literal
-            }
-       } 
-       //string part of RESP was empty 
-       return ""
-    }
-
-    private parseBulkString(tokens: Token[]) : string[] {
-        const result = []
-        for (let i = 0; i < tokens.length; i+= 1) {
-            const token = tokens[i]
-
-            //skip string length values
-            if (token.type === TokenType.DOLLAR) {
-                i = i + 2
-                continue
-            }
-
-            if (token.type === TokenType.STRING || token.type === TokenType.NUMBER) {
-                result.push(token.literal)
-            }
-        }
-
-        return result
     }
 
 }
