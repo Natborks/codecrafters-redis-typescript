2e3af9983fd63c94d63ff5aac1c8ca9302008a5b
natborks <nathanael@moonsquare.co>
2026-04-21 10:55:57 -0230

    codecrafters submit [skip ci]

--- app/utils/ResponseUtils.ts
+++ app/utils/ResponseUtils.ts
@@ -24,7 +24,7 @@ export default class ResponseUtils {
   ): string {
     let response = ""
     if (key) {
-      response = `*1\r\n*2\r\n${this.writeBulkString([key])}\r\n*1`
+      response = `*1\r\n*2\r\n${this.writeBulkString([key])}*1\r\n`
     }
     response.concat(`*${args.length}\r\n`)
 
