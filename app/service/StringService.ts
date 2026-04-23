dbce7ed44e2dbea80b4daf913b38eca832ac83e0
natborks <nathanael@moonsquare.co>
2026-04-23 11:57:15 -0230

    codecrafters submit [skip ci]

--- app/service/StringService.ts
+++ app/service/StringService.ts
@@ -98,6 +98,7 @@ export default class StringService {
   }
 
   discard(): string {
+    if (!this.execMode) return ResponseUtils.writeSimpleError("ERR DISCARD without MULTI")
     this.execQueue = []
     this.execMode = false
     return ResponseUtils.writeSimpleString("OK")
