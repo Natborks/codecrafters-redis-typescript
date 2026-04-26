116f1a3e63daa2137f97f1f15bfe71097d74d624
natborks <nathanael@moonsquare.co>
2026-04-26 21:05:57 -0230

    codecrafters submit [skip ci]

--- app/service/ReplicationService.ts
+++ app/service/ReplicationService.ts
@@ -10,6 +10,11 @@ export default class ReplicationService {
     if (!config) return ResponseUtils.writeSimpleError("No such service");
 
     if (config.isReplica) return ResponseUtils.writeBulkString(["role:slave"]);
+    console.log(ResponseUtils.writeBulkString([
+      "role:master",
+      `master_replid:${config.replid}`,
+      `master_repl_offset:"${config.replOffset}`,
+    ]))
     return ResponseUtils.writeBulkString([
       "role:master",
       `master_replid:${config.replid}`,
